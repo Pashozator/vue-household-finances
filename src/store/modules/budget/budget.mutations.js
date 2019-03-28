45e38fdd32c6533a00d897303464254d93e8cbff
Wojciech Szućko <wojciech.szucko@gmail.com>
2019-03-28 18:18:44 +0100

    fix: budget store

--- src/store/modules/budget/budget.mutations.js
+++ src/store/modules/budget/budget.mutations.js
@@ -20,28 +20,28 @@ export const budgetMutations = {
 	[GET_BUDGET] (state) {
 	},
 	[GET_BUDGET_SUCCESS] (state, payload) {
-		Vue.set(state, 'operations', { ...budgetAdapter.replaceAll(payload, state.operations) })
+		Vue.set(state, 'operations', budgetAdapter.replaceAll(payload, state.operations))
 	},
 	[GET_BUDGET_FAILURE] (state) {
 	},
 	[ADD_OPERATION] (state, payload) {
 	},
 	[ADD_OPERATION_SUCCESS] (state, payload) {
-		Vue.set(state, 'operations', { ...budgetAdapter.addOne(payload, state.operations) })
+		Vue.set(state, 'operations', budgetAdapter.addOne(payload, state.operations))
 	},
 	[ADD_OPERATION_FAILURE] (state) {
 	},
 	[EDIT_OPERATION] (state, payload) {
 	},
 	[EDIT_OPERATION_SUCCESS] (state, payload) {
-		Vue.set(state, 'operations', { ...budgetAdapter.updateOne({ id: payload.id, changes: payload }, state.operations) })
+		Vue.set(state, 'operations', budgetAdapter.updateOne({ id: payload.id, changes: payload }, state.operations))
 	},
 	[EDIT_OPERATION_FAILURE] (state) {
 	},
 	[REMOVE_OPERATION] (state, payload) {
 	},
 	[REMOVE_OPERATION_SUCCESS] (state, payload) {
-		Vue.set(state, 'operations', { ...budgetAdapter.removeOne(payload.id, state.operations) })
+		Vue.set(state, 'operations', budgetAdapter.removeOne(payload.id, state.operations))
 	},
 	[REMOVE_OPERATION_FAILURE] (state) {
 	},
